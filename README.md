6adc7740106b88e281e81e7da6cf26a4886bc01a
Miracle <datm2727@gmail.com>
2022-04-19 19:00:33 -0500

    Ahora se pueden descargar partidas de jugadores famosos desde chess.com

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 Descarga partidas en un solo pgn
 
-Busca colecciones en: https://www.chessgames.com/perl/collections
+Hay 3 formas de descargar PGNs:
+El imput que se le dará al programa debe ser igual al de los ejemplos
+
+1. Colecciones de ChessGames:
+Busca colecciones: https://www.chessgames.com/perl/collections
+Ejemplo de coleccion: https://www.chessgames.com/perl/chesscollection?cid=1033323
+
+2. Partidas de Jugadores Famosos (chessgames.com):
+Ejemplo: https://www.chessgames.com/perl/chessplayer?pid=52948
+
+3. Partidas de Jugadores Famosos (chess.com):
+Ejemplo: https://www.chess.com/players/mikhail-tal
--- chessgames.py
+++ chessgames.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,14 +9,14 @@ headers = {
 }
 
 
-def check_directory():
-    if not os.path.exists("ChessGames Collections"):
-        os.mkdir("ChessGames Collections")
+def check_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
 
 
-def check_file(filename):
+def check_file(folder_name, filename):
     # Entramos a la carpeta de la coleccion
-    os.chdir("ChessGames Collections")
+    os.chdir(folder_name)
 
     # Comprobamos si existe el fichero
     if os.path.isfile(filename):
@@ -26,15 +27,16 @@ def check_file(filename):
     os.chdir("..")
 
 
-def download_games(page_title, game_id_list):
-    check_directory()
+def download_games_chessgames(page_title, game_id_list):
+    folder_name = "ChessGames Collections"
+    check_folder(folder_name)
 
-    filename = "ChessGames Collections/" + page_title.replace(" ", "_") + ".pgn"
+    filename = folder_name + "/" + page_title.replace(" ", "_") + ".pgn"
 
     print("\nDownloading games from {}".format(page_title))
 
     # Comprobamos si existe el fichero
-    check_file(filename)
+    check_file(folder_name, filename)
 
     # Abrimos el fichero
     with open(filename, "w", encoding="utf-8") as f:
@@ -53,6 +55,50 @@ def download_games(page_title, game_id_list):
 
             position += 1
             new_percentage = int(position * 100 / len(game_id_list))
+
+            if new_percentage > current_percentage:
+                print("{}%".format(new_percentage))
+                current_percentage = new_percentage
+
+    print("\nGames downloaded successfully")
+
+
+def download_games_chesscom(page_title, game_id_list):
+    folder_name = "ChessCom Games"
+    check_folder(folder_name)
+
+    filename = folder_name + "/" + page_title.replace(" ", "_") + ".pgn"
+
+    print("\nDownloading games from {}".format(page_title))
+
+    # Comprobamos si existe el fichero
+    check_file(folder_name, filename)
+
+    # Dividimos la lista de Ids en grupos de 25
+    game_id_list_divided = [
+        game_id_list[i : i + 25] for i in range(0, len(game_id_list), 25)
+    ]
+
+    # Abrimos el fichero
+    with open(filename, "w", encoding="utf-8") as f:
+        current_percentage = 0
+        position = 0
+
+        for game_id_group in game_id_list_divided:
+            url_download = (
+                "https://www.chess.com/games/downloadPgn?game_ids="
+                + ",".join(str(game_id) for game_id in game_id_group)
+            )
+
+            response = requests.get(url_download, headers=headers)
+            # pgn = response.text.replace('\\"', "").replace("\r", " ")
+            pgn = response.text.replace('\\"', "").replace("\r", " ")
+
+            f.write(pgn)
+            f.write("\n\n\n\n")
+
+            position += 1
+            new_percentage = int(position * 100 / len(game_id_list_divided))
 
             if new_percentage > current_percentage:
                 print("{}%".format(new_percentage))
@@ -82,7 +128,7 @@ def option1_menu():
 
     page_title = soup.title.text
 
-    download_games(page_title, game_ids)
+    download_games_chessgames(page_title, game_ids)
 
     # Return to main menu
     main_menu()
@@ -126,7 +172,92 @@ def option2_menu():
 
         game_ids += [a_tag.attrs["href"].split("=")[-1] for a_tag in a_tags]
 
-    download_games(page_title, game_ids)
+    download_games_chessgames(page_title, game_ids)
+
+    # Return to main menu
+    main_menu()
+
+
+def option3_menu():
+    url = input("\nEnter the Url of the player: ")
+
+    # Validate user input
+    while not url.startswith("https://www.chess.com/players/"):
+        url = input(
+            "Invalid URL. Example: https://www.chess.com/players/mikhail-tal\nEnter the Url of the player: "
+        )
+
+    # Get the HTML of the player
+    response = requests.get(url, headers=headers)
+
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    # El botón que nos dirigirá a la página de las partidas del jugador
+    tag_games = soup.find("a", string=re.compile("Show All Games"))
+
+    # Nos redirigimos a la nueva página
+    url_games = tag_games.attrs["href"]
+    response = requests.get(url_games, headers=headers)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Título de la página
+    page_title = soup.title.text
+
+    # Buscamos el nav con la clase "ui_pagination-navigation"
+    nav_tag = soup.find("nav", class_="ui_pagination-navigation")
+
+    # Obtenemos la página 2
+    tag_page2 = nav_tag.select_one('a[href$="&page=2"]')
+
+    url_page2 = tag_page2.attrs["href"]
+
+    page_base = url_page2[:-1]
+
+    print("\nEtapa 1: Almacenar las IDs de todas las partidas\n")
+
+    game_id_list = []
+    index = 1
+
+    while True:
+        url_page = page_base + str(index)
+        response = requests.get(url_page, headers=headers)
+
+        if (
+            "Your search did not match any games. Please try a new search."
+            in response.text
+        ):
+            print("\nSe almacenaron todas las IDs de las partidas")
+            break
+
+        html = response.text
+        soup = BeautifulSoup(html, "html.parser")
+
+        # Buscamos el div con id "master-games-container"
+        div_tag = soup.find("div", id="master-games-container")
+
+        # Buscamos los "a" que contengan href con texto "https://www.chess.com/games/view/"
+        a_tags = div_tag.find_all(
+            "a", href=re.compile("https://www.chess.com/games/view/")
+        )
+
+        # Almacenamos las IDs de las partidas
+        local_id_list = [a_tag.attrs["href"].split("/")[-1] for a_tag in a_tags]
+        local_id_list = list(set(local_id_list))
+
+        game_id_list += local_id_list
+
+        print("Se obtuvieron {} IDs de partidas".format(len(game_id_list)))
+
+        index += 1
+
+    if len(game_id_list) == 0:
+        print("\nNo se encontraron partidas")
+    else:
+        print("\nEtapa 2: Descargar las partidas\n")
+
+        download_games_chesscom(page_title, game_id_list)
 
     # Return to main menu
     main_menu()
@@ -136,9 +267,10 @@ def main_menu():
     print(
         """
     --------------------------------------
-    1. Download Collection of Chess Games
-    2. Download Games from Player
-    3. Exit
+    1. Download Collection of Chess Games (chessgames.com)
+    2. Download Games from Player (chessgames.com)
+    3. Download Games from Player (chess.com)
+    4. Exit
     --------------------------------------
     """
     )
@@ -147,7 +279,7 @@ def main_menu():
     choice = input("Enter your choice: ")
 
     # Validate user input
-    while choice not in ["1", "2", "3"]:
+    while choice not in ["1", "2", "3", "4"]:
         choice = input("Invalid choice. Enter your choice: ")
 
     if choice == "1":
@@ -155,6 +287,8 @@ def main_menu():
     elif choice == "2":
         option2_menu()
     elif choice == "3":
+        option3_menu()
+    elif choice == "4":
         exit()
 
 
